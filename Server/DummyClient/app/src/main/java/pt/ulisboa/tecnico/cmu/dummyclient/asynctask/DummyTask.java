74cc9e4fb05a2d7b6223f14b153b886663ac322c
Jose Sa <joseagrsa@gmail.com>
2018-04-05 16:24:56 +0100

    Server

---
+++ Server/DummyClient/app/src/main/java/pt/ulisboa/tecnico/cmu/dummyclient/asynctask/DummyTask.java
@@ -0,0 +1,59 @@
+package pt.ulisboa.tecnico.cmu.dummyclient.asynctask;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import pt.ulisboa.tecnico.cmu.command.ResponseCommand;
+import pt.ulisboa.tecnico.cmu.dummyclient.MainActivity;
+import pt.ulisboa.tecnico.cmu.response.CommandResponse;
+
+public class DummyTask extends AsyncTask<String, Void, String> {
+
+    private MainActivity mainActivity;
+
+    public DummyTask(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    protected String doInBackground(String[] params) {
+        Socket server = null;
+        String reply = null;
+        ResponseCommand hc = new ResponseCommand(params[0]);
+        try {
+            server = new Socket("10.0.2.2", 9090);
+
+            ObjectOutputStream oos = new ObjectOutputStream(server.getOutputStream());
+            oos.writeObject(hc);
+
+            ObjectInputStream ois = new ObjectInputStream(server.getInputStream());
+            CommandResponse hr = (CommandResponse) ois.readObject();
+            reply = hr.getMessage();
+
+            oos.close();
+            ois.close();
+            Log.d("DummyClient", "Hi there!!");
+        }
+        catch (Exception e) {
+            Log.d("DummyClient", "DummyTask failed..." + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try { server.close(); }
+                catch (Exception e) { }
+            }
+        }
+        return reply;
+    }
+
+    @Override
+    protected void onPostExecute(String o) {
+        if (o != null) {
+            mainActivity.updateInterface(o);
+        }
+    }
+}
---
+++ Server/DummyClient/app/src/main/java/pt/ulisboa/tecnico/cmu/response/CommandResponse.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.cmu.response;
+
+public class CommandResponse implements Response {
+
+	private static final long serialVersionUID = 734457624276534179L;
+	private String message;
+	
+	public CommandResponse(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+}
