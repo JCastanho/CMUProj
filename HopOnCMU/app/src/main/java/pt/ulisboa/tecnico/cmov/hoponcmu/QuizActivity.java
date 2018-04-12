dc2ae38e901108be2e402fe70db194a44f8d8f41
marcelojcn <marcelojcnunes@hotmail.com>
2018-04-12 13:52:09 +0100

    added view for quizzes

---
+++ HopOnCMU/app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/QuizActivity.java
@@ -0,0 +1,44 @@
+package pt.ulisboa.tecnico.cmov.hoponcmu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        //TODO Remove after the mocks are not needed
+
+        //ADD TITLE
+        Bundle bundle = getIntent().getExtras();
+        TextView view = (TextView) findViewById(R.id.txtTitle);
+
+        view.setText(bundle.getString("Title"));
+
+
+        //ADD RESPONSES
+        RadioGroup group = (RadioGroup) findViewById(R.id.rdgResponses);
+
+        RadioButton btn;
+        for(int i = 1; i <= 4; i++){
+            btn = new RadioButton(this);
+            btn.setText("Response " + i);
+            group.addView(btn);
+        }
+
+        //ENDTODO
+
+    }
+    public void onSend(View view){
+        QuizActivity.this.finish();
+        Toast.makeText(this, "Answer Sent!", Toast.LENGTH_SHORT).show();
+    }
+}
